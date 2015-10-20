ddca12efd7eca3a3e005741621939d8285a1f485
ballistic.whisper <ballistic.whisper@decentralised-project.org>
2015-10-20 19:46:36 +0100

    Added cmake config.

--- src/decentralised/main.cpp
+++ src/decentralised/main.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include "dc-config.h"
 
 int main()
 {
-	printf("Gotta start sumwhere");
+	printf(std::string("Version ")
+		.append(std::to_string(Decentralised_VERSION_MAJOR))
+		.append(".")
+		.append(std::to_string(Decentralised_VERSION_MINOR))
+		.append("\n").c_str());
+
+	printf("Gotta start sumwhere\n");
+
 	std::string input = "";
 	getline(std::cin, input);
 
