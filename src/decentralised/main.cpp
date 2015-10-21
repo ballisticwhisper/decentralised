ab624268e75609e8b0e09c01cfcf845dd091a4b2
ballistic.whisper <ballistic.whisper@decentralised-project.org>
2015-10-21 22:59:30 +0100

    Linker errors fixed for referencing irrlicht objects.

--- src/decentralised/main.cpp
+++ src/decentralised/main.cpp
@@ -1,10 +1,34 @@
+#define _IRR_STATIC_LIB_
+
 #include <stdio.h>
 #include <string>
 #include <iostream>
 #include "dc-config.h"
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace irr::core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#ifndef _DEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+#pragma comment(lib, "irrlicht.lib")
+#endif
 
 int main()
 {
+	E_DRIVER_TYPE driverType = EDT_OPENGL;
+#if defined ( _IRR_WINDOWS_ )
+	driverType = EDT_DIRECT3D9;
+#endif
+
+	IrrlichtDevice * device = createDevice(driverType, irr::core::dimension2d<u32>(800, 600));
+
 	printf(std::string("Version ")
 		.append(to_string(Decentralised_VERSION_MAJOR))
 		.append(".")
